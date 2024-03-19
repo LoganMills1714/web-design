4bfb65ddc8f80d6a7dc5a534491b04c43055ad19
Logan Mills <millslogan1714@gmail.com>
2024-03-19 09:31:18 -0500

    Unit 2 Commit

---
+++ username.js
@@ -0,0 +1,28 @@
+var welcome = document.getElementById("welcome");
+
+function readCookie(cname) { 
+    var name = cname + "="; 
+    var decoded_cookie =  
+        decodeURIComponent(document.cookie); 
+    var carr = decoded_cookie.split(';'); 
+    for(var i=0; i<carr.length;i++){ 
+        var c = carr[i]; 
+        while(c.charAt(0)==' '){ 
+            c=c.substring(1); 
+        } 
+        if(c.indexOf(name) == 0) { 
+            return c.substring(name.length, c.length); 
+        } 
+    } 
+    return ""; 
+} 
+
+
+function runApp() { 
+    var user = readCookie("username"); 
+    if(user != ""){ 
+        welcome.innerHTML = "Welcome " + user
+    }
+}
+
+runApp()
